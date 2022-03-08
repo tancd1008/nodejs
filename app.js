6b9af4c2bbc08077cd8bdb6e5247ccb7ee3e6a02
tancd <caotan1008@gmail.com>
2022-03-08 18:13:18 +0700

    requesr and responve

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const http = require('http');
 // B2: Khởi tạo server
 const server = http.createServer((request, response) => {
     console.log(request.url);
+    if(request.url === "/"){
+        response.setHeader("Content-Type", "text/html");
+        response.write("<h1>Home Page</h1>");
+        response.end();
+    }
+    if(request.url === "/products"){
+        response.setHeader("Content-Type", "text/html");
+        response.write("<h1>Product Page</h1>");
+        response.end();
+    }
+    if(request.url === "/news"){
+        response.setHeader("Content-Type", "text/html");
+        response.write("<h1>New Page</h1>");
+        response.end();
+    }
 });
 // B3: Lắng nghe cổng thực thi
 const port = 3001;
