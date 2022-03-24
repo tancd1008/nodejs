3e08401a9474a6ba2e23b70f9df91d31a3f795ab
tancd <caotan1008@gmail.com>
2022-03-24 16:16:28 +0700

    Xoa cmt

--- controllers/products.js
+++ controllers/products.js
@@ -5,7 +5,6 @@ export const listProduct = async (request,response)=>{
     try{
         const product = await Product.find().exec()
         response.json(product)
-        // response.json(products)
     }catch(error){
         response.status(400).json({message:"Khong tim thay data"})
     }
@@ -14,12 +13,10 @@ export const listProductDetail = async (request,response)=>{
     try{
         const product = await Product.findOne({_id:request.params.id}).exec()
         response.json(product)
-        // response.json(products)
     }catch(error){
         response.status(400).json({message:"Khong tim thay data"})
     }
-    // const product = products.find(item => item.id === +request.params.id)
-    // response.json(product)
+   
 }
 export const createProduct = async (request,response)=>{
     try{
@@ -29,8 +26,7 @@ export const createProduct = async (request,response)=>{
     }catch(error){
         response.status(400).json({message:"Khong the tao moi"})
     }
-    // products.push(request.body)
-    // response.json(products)
+   
 }
 export const deleteProduct = async (request,response)=>{
     try {
@@ -39,8 +35,7 @@ export const deleteProduct = async (request,response)=>{
     } catch (error) {
         response.status(400).json({message:"Khong xoa duoc"})
     }
-    // const product = products.filter(item => item.id != request.params.id)
-    // response.json(product);
+    
 }
 export const updateProduct = async (request,response)=>{
     try {
@@ -49,5 +44,5 @@ export const updateProduct = async (request,response)=>{
     } catch (error) {
         response.status(400).json({message:"Loi khong update duoc"})
     }
-    // response.json(products.map(item => item.id === +request.params.id? request.body:item))
+    
 }
